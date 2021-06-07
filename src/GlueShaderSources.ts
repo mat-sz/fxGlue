e04707e8c24c8de9c11046989821fabe2a9e1898
Mat Sz <contact@matsz.dev>
2021-06-07 17:54:20 +0200

    replaced if-based clipping with clip

--- src/GlueShaderSources.ts
+++ src/GlueShaderSources.ts
@@ -8,7 +8,9 @@ export const defaultVertexShader = `void main() {
   gl_Position = vec4(position, 1.0);
 }`;
 
-export const blendFragmentShader = `uniform sampler2D iImage;
+export const blendFragmentShader = `@use wrap
+
+uniform sampler2D iImage;
 uniform vec2 iSize;
 uniform vec2 iOffset;
 uniform float iOpacity;
@@ -26,20 +28,19 @@ void main()
   vec4 src = texture2D(iTexture, p);
   gl_FragColor = src;
   
-  if (uv.x >= 0.0 && uv.y >= 0.0 && uv.x <= 1.0 && uv.y <= 1.0) {
-    vec4 dest = texture2D(iImage, uv);
-    dest.a *= iOpacity;
-    
-    if (iMode == 0) { // NORMAL
-      dest *= dest.a;
-      gl_FragColor *= 1.0 - dest.a;
-      gl_FragColor += dest;
-    } else if (iMode == 1) { // MULTIPLY
-      if (dest.a > 0.0) {
-        gl_FragColor.rgb = (dest.rgb / dest.a) * ((1.0 - src.a) + src.rgb);
-        gl_FragColor.a = min(src.a + dest.a - src.a * dest.a, 1.0);
-        // gl_FragColor.rgb *= mult.a;
-      }
+  vec4 dest = texture2D(iImage, uv);
+  dest.a *= iOpacity;
+  dest *= clip(uv);
+  
+  if (iMode == 0) { // NORMAL
+    dest *= dest.a;
+    gl_FragColor *= 1.0 - dest.a;
+    gl_FragColor += dest;
+  } else if (iMode == 1) { // MULTIPLY
+    if (dest.a > 0.0) {
+      gl_FragColor.rgb = (dest.rgb / dest.a) * ((1.0 - src.a) + src.rgb);
+      gl_FragColor.a = min(src.a + dest.a - src.a * dest.a, 1.0);
+      // gl_FragColor.rgb *= mult.a;
     }
   }
 }`;
