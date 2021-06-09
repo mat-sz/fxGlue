4478fcbf4be38197fe4ed212e15d32a165dce98c
Mat Sz <contact@matsz.dev>
2021-06-09 18:56:06 +0200

    added GlueUniformValue to main exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './Glue';
 export * from './GlueCanvas';
 export * from './GlueUtils';
 export { GlueBlendMode } from './GlueShaderSources';
+export { GlueUniformValue } from './GlueUniforms';
